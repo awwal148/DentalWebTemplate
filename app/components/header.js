5daeeebb9e82424567f2e5f74f6f9bcef08413f0
Awwal148 <adesanyaawwal446@gmail.com>
2025-10-27 18:11:34 +0000

    changed the logo name

--- app/components/header.js
+++ app/components/header.js
@@ -25,7 +25,7 @@ const Header = () => {
       <div className="container mx-auto flex items-center justify-between px-6 h-16">
         {/* Logo */}
         <div className="text-2xl font-bold text-blue-600">
-          <Link href="/">Dental</Link>
+          <Link href="/">Denti Belli</Link>
         </div>
 
         {/* Desktop Nav */}
